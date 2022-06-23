78dee599d1b8013a37fe2bd8b76100ca2e8218c5
Erika <erika.rasnick@gmail.com>
2022-06-23 11:59:26 -0400

    use direct output from census_block_group

--- entrypoint.R
+++ entrypoint.R
@@ -17,21 +17,60 @@ opt <- docopt::docopt(doc)
 ## for interactive testing
 ## opt <- docopt::docopt(doc, args = c('test/my_address_file_geocoded_census_block_group_0.5.1_2010.csv', 'test/census_mega_example.csv'))
 
-message("reading input file...")
-d1 <- readr::read_csv(opt$filename1, col_types = cols(fips_tract_id = col_character()))
-d2 <- readr::read_csv(opt$filename2, col_types = cols(fips_tract_id = col_character()))
+message("reading input files...")
+d1 <- suppressWarnings(
+  readr::read_csv(opt$filename1,
+                  col_types = cols(fips_tract_id_2020 = col_character(),
+                                   fips_tract_id_2010 = col_character(),
+                                   fips_tract_id_2000 = col_character(),
+                                   fips_tract_id_1990 = col_character(),
+                                   fips_tract_id_1980 = col_character(),
+                                   fips_tract_id_1970 = col_character(),
+                                   fips_block_group_id_2020 = col_character(),
+                                   fips_block_group_id_2010 = col_character(),
+                                   fips_block_group_id_2000 = col_character(),
+                                   fips_block_group_id_1990 = col_character()
+                                   )
+                  )
+  )
 
-dht::check_for_column(d1, "fips_tract_id", d1$fips_tract_id, "character")
-dht::check_for_column(d2, "fips_tract_id", d2$fips_tract_id, "character")
+d2 <- readr::read_csv(opt$filename2, col_types = list(col_character()))
 
-n_both <- length(intersect(d1$fips_tract_id[!is.na(d1$fips_tract_id)], d2$fips_tract_id[!is.na(d2$fips_tract_id)]))
-n_d1 <- length(unique((d1$fips_tract_id[!is.na(d1$fips_tract_id)])))
-n_d2 <- length(unique((d2$fips_tract_id[!is.na(d2$fips_tract_id)])))
+f1_name <- names(d1)[substr(names(d1), 1, 13) == "fips_tract_id"]
+f2_name <- names(d2)[1]
+names(d2)[1] <- f1_name
+
+if(f1_name < 1) {
+  cli::cli_alert_error("File 1 does not contain a column name matching output from the DeGAUSS census_block_group_container.")
+}
+
+cli::cli_alert_info("Data will be joined using the {f1_name} column from file 1 and the {f2_name} column from file 2")
+
+
+
+n_both <- length(intersect(d1 %>%
+                             dplyr::select({{f1_name}}) %>%
+                             dplyr::filter(!is.na(.)) %>%
+                             pull(),
+                           d2 %>%
+                             dplyr::select({{f1_name}}) %>%
+                             dplyr::filter(!is.na(.)) %>%
+                             pull()))
+
+n_d1 <- length(unique((d1 %>%
+                         dplyr::select({{f1_name}}) %>%
+                         dplyr::filter(!is.na(.)) %>%
+                         pull())))
+
+n_d2 <- length(unique((d2 %>%
+                         dplyr::select({{f1_name}}) %>%
+                         dplyr::filter(!is.na(.)) %>%
+                         pull())))
 
 cli::cli_alert_info("There are {n_d1} unique tract ids in dataset 1, {n_d2} unique tract ids in dataset 2, and {n_both} unique tract ids present in both datasets.")
 
 ## add code here to calculate geomarkers
-d <- left_join(d1, d2, by = "fips_tract_id")
+d <- left_join(d1, d2, by = {{f1_name}})
 
 file_name_merged <- glue::glue("{stringr::str_remove(opt$filename1, '.csv')}_{basename(opt$filename2)}")
 
